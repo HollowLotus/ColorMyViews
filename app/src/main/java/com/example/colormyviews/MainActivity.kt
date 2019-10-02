96ee1270257df8da19a4fbeb72a07a77707e18ab
Allan Atton <54817371+HollowLotus@users.noreply.github.com>
2019-10-01 21:58:40 -0700

    updated TextView id's

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun setListeners() {
         val clickableViews: List<View> =
-            listOf(box_one_text, box_two_text, box_three_text, box_four_text, box_five_text)
+            listOf(ConstraintLayout, box_two_text, box_three_text, box_four_text, box_five_text)
 
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         when (view.id) {
 
             // Boxes using Color class colors for background
-            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.ConstraintLayout -> view.setBackgroundColor(Color.DKGRAY)
             R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
 
             // Boxes using Android color resources for background
